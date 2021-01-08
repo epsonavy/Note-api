587208defebbb477ccfdb0b3401a4bf8667d9744
epso <epsonavy@yahoo.com>
2021-01-08 12:33:35 -0800

    fix error on path / and add AuthController to handle new user register and getToken

---
+++ api/src/Controller/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthController extends ApiController
+{
+
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->transformJsonBody($request);
+        $email = $request->get('email');
+        $password = $request->get('password');
+
+
+        if (empty($email) || empty($password)) {
+            return $this->respondValidationError("Invalid Email or Password");
+        }
+
+        $user = new User($email);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $em->persist($user);
+        $em->flush();
+        return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    /**
+     * @param UserInterface $user
+     * @param JWTTokenManagerInterface $JWTManager
+     * @return JsonResponse
+     */
+    public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+    {
+        return new JsonResponse(['token' => $JWTManager->create($user)]);
+    }
+
+}
