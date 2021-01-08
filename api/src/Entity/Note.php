cc828490bc49dca2459b2dcd7a5804016019d207
epso <epsonavy@yahoo.com>
2021-01-08 11:32:47 -0800

    add note entity and api resource route by annoation

---
+++ api/src/Entity/Note.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Note
+{
+    /**
+     * @var int The id of a note.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The title of the note.
+     *
+     * @ORM\Column(type="text", length=50)
+     */
+    public $title;
+
+    /**
+     * @var string The content of the note.
+     *
+     * @ORM\Column(type="text", length=1000)
+     */
+    public $content;
+
+    /**
+     * @var \DateTimeInterface When the note was updated.
+     *
+     * @ORM\Column(type="datetime")
+     */
+    public $updatedAt;
+
+    /**
+     * @var \DateTimeInterface When the note was created.
+     *
+     * @ORM\Column(type="datetime")
+     */
+    public $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
